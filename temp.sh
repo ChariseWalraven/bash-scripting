caea3b3757069676c699155cfd7b63b127a18a7b
Charise Walraven <cherri.walraven@gmail.com>
2019-09-06 10:57:12 +0200

    remove some bash

--- temp.sh
+++ temp.sh
@@ -1,23 +1,2 @@
-#!/bin/bash
-repos=(
-  "CZ-Prod"
-  "Just-Test"
-  "Just-Accp"
-  "Just-Prod"
-  "PZP-Test"
-  "PZP-Accp"
-  "PZP-Prod"
-)
-
-echo "changing to correct directory"
-cd ~/code/cz/cordova-plugin-onegini-config
-echo "In directory $PWD"
-
-echo "starting loop"
-for repo in  "${repos[@]}"; do
-  echo "checking out $repo"
-  git checkout $repo
-  git status
-  echo "pushing to origin $repo"
-  git push -u origin $repo
-done
+#!/usr/bin/env bash
+# temporary file used to automate boring tasks
