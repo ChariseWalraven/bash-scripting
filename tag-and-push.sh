f7e305cd1e0b72428f2299fb6610729c6915c112
Charise Walraven <cherri.walraven@gmail.com>
2019-09-06 11:21:47 +0200

    add some colours

--- tag-and-push.sh
+++ tag-and-push.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 # desired use: tap <tagname>
 
-# 1. check number of arguments. Should be only one. If more, abort and log error message in (light) red. else continue.
 current_branch=`git rev-parse --abbrev-ref HEAD`
 remote=`git remote`
+# there should only be one argument
+# TODO: consider colours. Should I colour code the results? Definitely not necessary, but might be a nice touch, and also helps to communicate if something went well or wrong. Also, how far do we take this?
 if [ $# -eq 1 ]
   then 
-  # a. Delete remote tags by name supplied
-    echo "Deleting tag $1 on remote repo..."
+    echo -e "\e[33mDeleting tag $1 on remote repo...\e[39m"
     git push $remote :refs/tags/$1
-  # b. Delete local tags by name supplied
-    echo "Deleting $1 on local repo..."
+    echo -e "\e[33mDeleting $1 on local repo...\e[39m"
     git tag -d $1
-  # c. tag locally
-    echo "Tagging most recent commit on local branch with tag $1"
+    echo -e "\e[33mTagging most recent commit on local branch with tag $1...\e[39m"
     git tag $1
-  # d. push changes to remote with tags
-    echo "Pushing commits and tags to branch $remote $current_branch"
+    echo -e "\e[32mDone.\e[39m"
+    echo -e "\e[33mPushing commits and tags to branch $remote $current_branch...\e[39m"
     git push -u $remote $current_branch refs/tags/$1
+    echo -e "\e[1;34mFinished.\e[39m"
   else 
-    echo "Wrong number of arguments supplied. You must only supply the name of the tag you wish to have moved to the current commit and pushed to the current branch on the remote repository."
+    echo -e "\e[91mWrong number of arguments supplied. You must only supply the name of the tag you wish to have moved to the current commit and pushed to the current branch on the remote repository.\e[39m"
 fi
